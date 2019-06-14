97ad83a966fbef9901ff5518f9e70fecaf664263
EmosewaPixel <awesomeggaming@gmail.com>
2019-06-14 20:05:29 +0200

    Git forgot the most important part

--- src/main/java/com/EmosewaPixel/pixellib/materialSystem/element/ElementalProperties.java
+++ src/main/java/com/EmosewaPixel/pixellib/materialSystem/element/ElementalProperties.java
@@ -1,4 +1,33 @@
 package com.EmosewaPixel.pixellib.materialSystem.element;
 
 public class ElementalProperties {
+    private String symbol;
+    private int protons;
+    private int neutrons;
+
+    public ElementalProperties(String symbol, int protons, int neutrons) {
+        this.symbol = symbol;
+        this.protons = protons;
+        this.neutrons = neutrons;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getProtons() {
+        return protons;
+    }
+
+    public int getNeutrons() {
+        return neutrons;
+    }
+
+    public double getAtomicMass() {
+        return protons * 1.00782458 + neutrons * 1.008664;
+    }
+
+    public double getDensity() {
+        return getAtomicMass() /  22.414;
+    }
 }
