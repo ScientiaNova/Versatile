fb36df4426bedfde526a2d2a18215baab511b261
Dylan <quantum64development@gmail.com>
2019-12-29 03:14:00 -0500

    One line folding

--- src/main/kotlin/com/scientianovateam/versatile/common/loaders/internal/PriorityJsonLoader.kt
+++ src/main/kotlin/com/scientianovateam/versatile/common/loaders/internal/PriorityJsonLoader.kt
@@ -6,17 +6,14 @@ import com.scientianovateam.versatile.common.extensions.merge
 import kotlin.math.max
 import kotlin.math.min
 
-fun cascadeJsons(resources: List<JsonObject>): JsonObject {
-    resources.forEach { json ->
-        val priority = json.get("priority")
-        val namespace = json.get("namespace").asString!!
-        val updated = min(PriorityStore.maximums[namespace] ?: Int.MAX_VALUE,
-                max(PriorityStore.minimums[namespace] ?: Int.MIN_VALUE,
-                        when {
-                            priority is JsonPrimitive && priority.isNumber -> priority.asInt
-                            else -> 3
-                        }))
-        json.addProperty("priority", updated)
-    }
-    return resources.fold(JsonObject(), { a, b -> a.merge(b) })
-}
+fun cascadeJsons(resources: List<JsonObject>): JsonObject = resources.onEach { json ->
+    val priority = json.get("priority")
+    val namespace = json.get("namespace").asString!!
+    val updated = min(PriorityStore.maximums[namespace] ?: Int.MAX_VALUE,
+            max(PriorityStore.minimums[namespace] ?: Int.MIN_VALUE,
+                    when {
+                        priority is JsonPrimitive && priority.isNumber -> priority.asInt
+                        else -> 3
+                    }))
+    json.addProperty("priority", updated)
+}.fold(JsonObject()) { a, b -> a.merge(b) }
